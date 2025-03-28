08c47dbeef56973e659cad7c055fe3e5075a7461
lunarewa <lunarewa@gmail.com>
2025-03-28 17:55:39 +0300

    Home work #1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         printThreeWords();
         checkSumSign();
         printColor();
